8ba5f237a31bcd7eb3a7b34eb1fb0e9de5e8838a
Devansh Bindlish <devanshbindlish999@gmail.com>
2020-07-19 15:41:34 +0530

    User Registeration Auth Added

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,24 @@ var express = require("express");
 var router = express.Router();
 var User = require('../model/User')
 
+//var
+var Joi = require('@hapi/joi');
+
+var schema = Joi.object({
+    name:Joi.string().min(6).required(),
+    email:Joi.string().min(6).required().email(),
+    password:Joi.string().min(6).required()
+
+})
+
 router.post('/register',async (req,res)=>{
+
+    //Validating user
+    var {error} = schema.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    // if(validation)
+    // res.send(validation)
+
     var user = new User({
         name:req.body.name,
         email:req.body.email,
@@ -14,7 +31,7 @@ router.post('/register',async (req,res)=>{
     }catch(err){
         res.status(400).send(err)
     }
-})
+ })
 
 // router.post('/login',(req,res)=>{
 
